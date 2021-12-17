1710f16f2bab4970d2274c408f10c9989dba3f9d
Jesus Conde <educaleft@gmail.com>
2021-12-17 06:03:56 +0000

    Agregar archivos de proyecto.

---
+++ BloqueIf/BloqueIf.cpp
@@ -0,0 +1,226 @@
+#include <iostream>
+
+int main()
+{
+	if (true)
+	{
+		int x{ 5 };
+	}
+	else
+	{
+		int x{ 6 };
+	}
+
+	std::cout << x;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+/*#include <iostream>
+int main()
+{
+	int x{ 10 };
+	if (x > 10)
+	{
+
+		std::cout << x << "es mayor que 10\n";
+	}
+	else
+	{
+		std::cout << x << "no es mayor que 10\n";
+	}
+	return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+int a{};
+int b{};
+int c{};
+void sumar(int a, int b) {
+	c = a + b;
+	std::cout << c;
+}
+int main()
+{
+	std::cout << "Dinos tu edad : ";
+	int x{};
+	std::cin >> x;
+
+	if (x >= 18)
+		std::cout << "Tienes edad para inscribirte.\n";
+	sumar(4, 5);
+
+
+	return 0;
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+int main()
+{	
+	int x{10};
+	if (x > 10) 	
+		std::cout << x << "es mayor que 10\n";	
+	else	
+		std::cout << x << "no es mayor que 10\n";
+	return 0;
+}
+
+
+
+
+
+#include <iostream>
+
+int main()
+{
+	if (true)
+		int x{ 5 };
+	else
+		int x{ 6 };
+
+	std::cout << x;
+
+	return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+int a{};
+int b{};
+int c{};
+void sumar(int a, int b) {
+	c = a + b;
+	std::cout << c;
+}
+int main()
+{
+	std::cout << "Dinos tu edad : ";
+	int x{};
+	std::cin >> x;
+
+	if (x >= 18)
+		//std::cout << "Tienes edad para inscribirte.\n";
+	sumar(4, 5);		
+
+
+	return 0;
+
+}*/
+
+/*#include <iostream>
+
+int main()
+{
+	std::cout << "Dinos tu edad : ";
+	int x{};
+	std::cin >> x;
+
+	if (x >= 18)
+		std::cout << "Tienes edad para inscribirte.\n";
+	else
+	{
+		std::cout << "No tienes edad para inscribirte.\n";
+		std::cout << "Demasiado joven\n"; // fíjate en esta línea
+	}
+
+	return 0;
+}*/
+
+
+
+/*#include <iostream>
+
+int main()
+{
+	std::cout << "Dinos tu edad : ";
+	int x{};
+	std::cin >> x;
+
+	if (x >= 18)
+		std::cout << "Tienes edad para inscribirte.\n";
+	else
+		std::cout << "No tienes edad para inscribirte.\n";
+	std::cout << "Demasiado joven\n"; // fíjate en esta línea
+
+	return 0;
+}*/
+
+
+/*#include <iostream>
+
+int main()
+{
+	std::cout << "Escribe un número: ";
+	int x{};
+	std::cin >> x;
+
+	if (x > 10)
+		std::cout << x << "es mayor que 10\n";
+	else
+		std::cout << x << "no es mayor que 10\n";
+
+	return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
